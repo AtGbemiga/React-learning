ccee65fc1daa7fefe1a445975642a8e3e88c21bf
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-03-16 18:13:18 +0100

    Add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React from "react"
+import ReactDOM, { render } from "react-dom"
 import './App.css';
 
-function App() {
+// heading
+function Hone () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>react practice</h1>
+    </>
+  )
 }
 
-export default App;
+// this button changes state on click
+class Btn extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        change: "Not Clicked :-(" 
+      };
+      this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick () {
+      this.setState ({change: "Yaa! Clicked :-)"})
+    }
+    render() {
+      return (
+        <>
+          <button onClick={this
+          .handleClick
+        }>Click Me</button>
+        <h3>{this.state.change}</h3>
+        </>
+      )
+    }
+}
+
+// toggle effect with btn
+class Toggle extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {xyz: false}
+    this.toggleEffect = this.toggleEffect.bind(this)
+  }
+  toggleEffect() {
+    this.setState(state=> {
+      if(state.xyz === true) {
+        return {xyz: false}
+      } else {
+        return {xyz: true}
+      }
+    });
+  }
+  render() {
+    if(this.state.xyz) {
+      return (
+        <>
+          <button onClick = {this.toggleEffect}>Click Me Too</button>
+          <h3>I am shown. Whoo Whoo! I'm a toggle button.</h3>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <button onClick = {this.toggleEffect}>Click Me Too</button>
+        </>
+      )
+    }
+    
+  } 
+}
+// The function that renders all components
+function MainBody () {
+    return (
+      <>
+        <Hone />
+        <Btn />
+        <Toggle />
+      </>
+    )
+}
+
+export default MainBody;
