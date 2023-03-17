fbff6a79c2640c5b187388cd4410393821c421a4
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-03-17 16:25:05 +0100

    Add more practice. Mock API, input, etc

--- src/App.js
+++ src/App.js
@@ -69,15 +69,143 @@ class Toggle extends React.Component{
     
   } 
 }
-// The function that renders all components
-function MainBody () {
+
+//Increment, etc
+class ChangeValue extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {count: 0}
+    this.increase = this.increase.bind(this)
+    this.decrease = this.decrease.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+  reset() {
+    this.setState({count: 0})
+  }
+  increase() {
+    this.setState(state => ({count: state.count + 1}) )
+  }
+  decrease() {
+    this.setState(state => ({count: state.count - 1}) )
+  }
+  render() {
+    return(
+      <>
+      <br />
+        <button onClick={this.increase}>Increase</button>
+        <button onClick={this.decrease}>Decrease</button>
+        <button onClick={this.reset}>Reset</button>
+        <h1>Count: {this.state.count}</h1>
+      </>
+    )
+  }
+}
+
+// log input state
+class Input extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {myInput: ''}
+    this.myMethod = this.myMethod.bind(this)
+  }
+  myMethod(event) {
+    this.setState({myInput: event.target.value})
+  }
+  render() {
+    return(
+      <>
+        <input value={this.state.myInput} onChange={this.myMethod}></input>
+        <h3>You typed: <strong>{this.state.myInput}</strong></h3>
+        <Returnprops myInput={this.state.myInput}/>
+      </>
+    )
+  }
+}
+
+// return property from another component
+class Returnprops extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h3>{this.props.myInput} - I returned this property from another component</h3>
+    )
+  }
+}
+
+// serve stimulation
+class Server extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {onlineUsers: null}
+  }
+  componentDidMount() {
+    setTimeout(()=> 
+    this.setState({onlineUsers: 4000000})
+    )
+  }
+  render() {
     return (
       <>
+        <h3>API call stimulation</h3>
+        <h3>Online users: {this.state.onlineUsers}</h3>
+      </>
+    )
+  }
+}
+
+// keyboard event. PROBLEM HERE. CAN'T DISPLAY
+class KeyboardEventC extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // Change code below this line
+  componentDidMount() {
+    document.addEventListener("keydown", this.setState({message: this.handleKeyPress}))
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.setState({message: this.handleKeyPress}))
+  }
+  // Change code above this line
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! '
+    }));
+  }
+  handleKeyPress(event) {
+    if (KeyboardEvent.code === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
+
+// The function that renders all components
+function Main () {
+    return (
+      <div className="css">
         <Hone />
         <Btn />
         <Toggle />
-      </>
+        <ChangeValue />
+        <Input />
+        <Server />
+      </div>
     )
 }
 
-export default MainBody;
+
+
+export default Main;
